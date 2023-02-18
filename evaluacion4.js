03246608a46d46f885d4ad46fa48cf62a2fe722b
Yefferson777 <102541257+Yefferson777@users.noreply.github.com>
2023-02-18 15:45:36 -0400

    Add files via upload

---
+++ evaluacion4.js
@@ -0,0 +1,17 @@
+let edad = 19;
+
+if (edad < 14){
+    console.log("ES UN NIÑO");
+}
+else{
+    if (edad < 23){
+        console.log("ES UN ADOLECENTE");
+    }
+    else{
+        if (edad < 48){
+            console.log("ES UN ADULTO");
+        }else{
+            console.log("ES UN ADULTO MAYOR");
+        }
+    }
+}
